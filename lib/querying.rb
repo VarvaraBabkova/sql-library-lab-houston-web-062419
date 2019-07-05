d63c273af83e5622a6b865b5cf669f2b07c71688
Varvara Babkova <varvara.munday@gmail.com>
2019-07-05 17:09:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 
 def select_series_title_with_most_human_characters
   #{}"select title from books, characters, character_books where characters.species = 'human';"
-  "select title, count(species) from books, characters, character_books where characters.species = 'human' group by species
+  "select title, count(species) from books, characters, character_books where characters.species = 'human' group by title
    order by count(species) desc limit 1;"
 
 end
